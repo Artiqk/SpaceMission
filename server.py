d5e9ba366352a11fc216e35228ae67396d8c4d12
antoine <antoineabonneau94@gmail.com>
2022-05-01 16:44:53 -0400

    Added array to display data from mysql

--- server.py
+++ server.py
@@ -25,6 +25,42 @@ def create_mission ():
 def create_user ():
     return render_template('create_user.html')
 
+@app.route('/display_missions')
+def display_missions():
+    cursor = mysql.connection.cursor()
+    sql_request = "SELECT mission_name, mission_description FROM missions;"
+    cursor.execute(sql_request)
+    missions = cursor.fetchall()
+
+    html_data = ""
+
+    for all_missions in missions:
+        html_data += "<tr>"
+        for mission in all_missions:
+            html_data += "<td>" + str(mission) + "</td>"
+        html_data += "</tr>"
+
+    return render_template('display_missions.html', data=html_data)
+
+@app.route('/display_users')
+def display_users():
+    cursor = mysql.connection.cursor()
+    sql_request = "SELECT first_name, last_name, age, mail FROM users;"
+    cursor.execute(sql_request)
+    users = cursor.fetchall()
+
+    html_data = ""
+
+    print(users)
+
+    for all_user in users:
+        html_data += "<tr>"
+        for user in all_user:
+            html_data += "<td>" + str(user) + "</td>"
+        html_data += "</tr>"
+
+    return render_template('display_users.html', data=html_data)
+
 @app.route('/create_missions', methods=['GET', 'POST'])
 def create_missions (): 
     post_data_name = ['mission_name', 'mission_desc']
@@ -33,7 +69,7 @@ def create_missions ():
     cursor.execute(sql_request)
     mysql.connection.commit()
     cursor.close()  
-    return "<p>" + sql_request + "</p>"
+    return render_template('create_mission.html')
 
 @app.route('/create_users', methods=['GET', 'POST'])
 def create_users (): 
@@ -43,4 +79,4 @@ def create_users ():
     cursor.execute(sql_request)
     mysql.connection.commit()
     cursor.close()  
-    return "<p>" + sql_request + "</p>"
+    return render_template('create_user.html')
