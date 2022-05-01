5b6c55412bbb86e589684a514aa604d61ff29c05
antoine <antoineabonneau94@gmail.com>
2022-05-01 15:12:42 -0400

    Fixed setup bis

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 pip install flask
-pip install flask-mysqldb
+sudo apt install python3-flask
 sudo apt install default-libmysqlclient-dev
 sudo apt install mariadb-server
+pip install flask-mysqldb
 sudo systemctl start mariadb
 sudo mysql -u root < createBDD.sql
