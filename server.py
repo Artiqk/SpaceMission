2d5c5b791183962828e1df09152d7fe0a7a88ba4
antoine <antoineabonneau94@gmail.com>
2022-05-01 13:08:00 -0400

    Added proper routes to html files

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
+from django.shortcuts import render
 from flask import Flask, render_template
 import os
 
 
 project_path = os.path.dirname(__file__)
 
-app = Flask(__name__, template_folder=project_path)
+app = Flask(__name__)
 
 @app.route("/")
-
 def index():
     return render_template('index.html')
+
+@app.route('/create_mission')
+def create_mission ():
+    return render_template('create_mission.html')
+
+@app.route('/create_user')
+def create_user ():
+    return render_template('create_user.html')
